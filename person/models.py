649c14d6105d165eb44247e814e52baab76b3b59
Meng Zhuo <mengzhuo1203@gmail.com>
2012-11-15 14:14:28 +0800

    fix models follow

--- person/models.py
+++ person/models.py
@@ -9,6 +9,9 @@ class Person( models.Model ):
     name = models.CharField(max_length=8,
                             verbose_name=u'用户名'
                             )
-    
+    follow = models.ManyToManyField(
+                     'self', related_name = 'follower',
+                    symmetrical=False,blank=True,null=True)
+
     def __unicode__(self):
         return self.name
